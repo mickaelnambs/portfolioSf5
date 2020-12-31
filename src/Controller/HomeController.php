546a62392adc480cc01a6a7f7ab12e744d52c940
Mickael Nambinintsoa <mickael.nambinintsoa07081999@gmail.com>
2020-12-31 14:10:54 +0300

    Home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HomeController.
+ * 
+ * @author Mickael Nambinintsoa <mickael.nambinintsoa07081999@gmail.com>
+ */
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home", methods={"POST","GET"})
+     */
+    public function index()
+    {
+        return $this->render('front/home/index.html.twig');
+    }
+}
