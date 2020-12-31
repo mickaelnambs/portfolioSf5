6d030bf0b47c0604199a9f51882769badb1e4c76
Mickael Nambinintsoa <mickael.nambinintsoa07081999@gmail.com>
2020-12-31 13:58:37 +0300

    initial commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Formation;
+use App\Entity\Media;
+use App\Entity\Reference;
+use App\Entity\Skill;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    /**
+     * AppFixtures constructor.
+     * @param UserPasswordEncoderInterface $userPasswordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $manager->flush();
+    }
+}
